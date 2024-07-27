7e8e60d8e20154e23366abd42edbf7572030ca83
Zirui Li <le13851690127@gmail.com>
2024-07-27 17:16:50 -0400

    prepare global_pose publisher for testing rtabmap localization

--- scripts/simulator_node.py
+++ scripts/simulator_node.py
@@ -12,24 +12,29 @@ import tf.transformations
 import numpy as np
 
 from std_msgs.msg import Header
-from geometry_msgs.msg import Pose, PoseStamped, Twist, Quaternion, TransformStamped
+from geometry_msgs.msg import Pose, PoseStamped, Twist, Quaternion, TransformStamped, PoseWithCovarianceStamped
 from sensor_msgs.msg import CameraInfo, Image
 from nav_msgs.msg import Odometry, OccupancyGrid
 
 
 class SimulatorNode:
     def __init__(self, dataset_name, test_scene, test_scene_name, dt=0.01, init_x=0.0, init_y=0.0, init_theta=0.0) -> None:
-        self.sim = sim.Simulator(dataset_name=dataset_name, test_scene=test_scene, test_scene_name=test_scene_name)
-        self.robot = systems.Unicycle3D(init_x=init_x, init_y=init_y, init_theta=init_theta, dt=dt)
+        self.sim = sim.Simulator(dataset_name=dataset_name, test_scene=test_scene, test_scene_name=test_scene_name,
+                                 initial_state=[init_x, init_y, init_theta], dt=dt)
+        # self.robot = systems.Unicycle3D(dt=dt)
         self.bridge = cv_bridge.CvBridge()
 
+        self.init_x = init_x
+        self.init_y = init_y
+        self.init_theta = init_theta
         self.robot_pose_pub = rospy.Publisher("gt_pose", Pose, queue_size=10)
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
         self.robot_view_rgb_pub = rospy.Publisher("rgb/image", Image, queue_size=10)
         self.robot_view_depth_pub = rospy.Publisher("depth/image", Image, queue_size=10)
         self.camera_info_pub = rospy.Publisher("rgb/camera_info", CameraInfo, queue_size=10)
         self.map_pub = rospy.Publisher("floor_plan", OccupancyGrid, queue_size=10)
-        
+        self.global_pose_pub = rospy.Publisher("global_pose", PoseWithCovarianceStamped, queue_size=10)
+
         self.cmd_vel_sub = rospy.Subscriber("cmd_vel", Twist, callback=self.cmd_vel_callback)
         self.goal_sub = rospy.Subscriber("goal", PoseStamped, callback=self.goal_callback)
 
@@ -45,12 +50,11 @@ class SimulatorNode:
         v = msg.linear.x
         w = msg.angular.z
         self.vel = [v, w]
-        self.robot.step(v, w)
+        self.sim.agent_step(v, w)
 
     def update_sim(self):
-        state = self.robot.state
-        img = self.sim.get_rgb_observation(state[0], state[1], state[2])
-        depth = self.sim.get_depth_observation(state[0], state[1], state[2])
+        img = self.sim.get_rgb_observation()
+        depth = self.sim.get_depth_observation()
         # topdown_view = sim.get_topdown_view()
 
         self.publish_floorplan()
@@ -58,8 +62,11 @@ class SimulatorNode:
         self.publish_rgb_img(img)
         self.publish_depth_img(depth)
         self.publish_camera_info()
+        # self.publish_global_pose()
         self._cameralink_to_baselink_tf()
-        self._odom_to_baselink_tf(state[0], state[1], state[2])
+        self._map_to_odom_tf()
+
+        self._odom_to_baselink_tf(*self.sim.get_agent_state_odom())
 
     def publish_floorplan(self):
         floorplan = OccupancyGrid()
@@ -89,6 +96,8 @@ class SimulatorNode:
         caminfo_msg = self._construct_camera_info_message(K)
         self.camera_info_pub.publish(caminfo_msg)
 
+
+
     def publish_rgb_img(self, img):
         header = Header()
         header.stamp = rospy.Time.now()
@@ -104,9 +113,29 @@ class SimulatorNode:
         depth_msg = self.bridge.cv2_to_imgmsg(depth, encoding="passthrough", header=header)
         self.robot_view_depth_pub.publish(depth_msg)
 
+    def publish_global_pose(self):
+        pose = PoseWithCovarianceStamped()
+
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = "map"
+
+        x, y, theta = self.sim.robot_state_world
+
+        pose.pose.pose.position.x = x
+        pose.pose.pose.position.y = y
+        pose.pose.pose.position.z = 0.0  # Assuming 2D plane
+
+        quat = tf.transformations.quaternion_from_euler(0, 0, theta)
+        pose.pose.pose.orientation = Quaternion(*quat)
+
+        # Set covariance (assuming zero covariance for simplicity, adjust as needed)
+        pose.pose.covariance = [0.0] * 36
+
+        self.global_pose_pub.publish(pose)
+
     def publish_robot_odom(self):
-        state = self.robot.state
-        odom_msg = self._construct_odom_message(state[0], state[1], state[2])
+        state = self.sim.robot.state
+        odom_msg = self._construct_odom_message(*self.sim.get_agent_state_odom())
         self.odom_pub.publish(odom_msg)
 
     def _construct_odom_message(self, x, y, theta):
@@ -167,6 +196,23 @@ class SimulatorNode:
         
         return camera_info
 
+    def _map_to_odom_tf(self):
+        static_transform = TransformStamped()
+
+        static_transform.header.stamp = rospy.Time.now()
+        static_transform.header.frame_id = "map"
+        static_transform.child_frame_id = "odom"
+
+        static_transform.transform.translation.x = self.init_x
+        static_transform.transform.translation.y = self.init_y
+        static_transform.transform.translation.z = 0.0
+
+        quat = tf.transformations.quaternion_from_euler(0, 0, self.init_theta)
+
+        static_transform.transform.rotation = Quaternion(*quat)
+
+        self.broadcaster.sendTransform(static_transform)
+
     def _cameralink_to_baselink_tf(self):
         static_transform = TransformStamped()
 
@@ -201,13 +247,14 @@ class SimulatorNode:
 
         self.broadcaster.sendTransform(transform)
 
+
 def setup_data():
     dir_path = str(Path(__file__).parent.parent)
     with open(os.path.join(dir_path, 'config/path_config.json')) as path_config_file:
         path_config = json.load(path_config_file)
 
     data_root = path_config['data_root']
-    dataset_name = 'hssd'
+    dataset_name = 'hssd-hab'
     scene_idx = 2
     if dataset_name == 'hssd':
         scene_map = {0: "102344469", 1: "102344022", 2: "102344094", 3: "103997403_171030405", 4: "102815859", 5:"102816216"}
@@ -229,7 +276,7 @@ rospy.init_node("simulator")
 rate = rospy.Rate(10)
 
 dataset_name, test_scene, test_scene_name = setup_data()
-sim_node = SimulatorNode(dataset_name=dataset_name, test_scene=test_scene, test_scene_name=test_scene_name, init_x=0, init_y=0, init_theta=0.0)
+sim_node = SimulatorNode(dataset_name=dataset_name, test_scene=test_scene, test_scene_name=test_scene_name, init_x=1, init_y=0, init_theta=1.5)
 
 while not rospy.is_shutdown():
     sim_node.update_sim()
